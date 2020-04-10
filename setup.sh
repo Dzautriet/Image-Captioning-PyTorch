e2a447be161889b79601e643717f0369e1ba3982
Zhe <tsauhdet@gmail.com>
2020-04-10 20:09:44 +0100

    fixed error in .sh file

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ cp kaggle.json ~/.kaggle/
 chmod 600 ~/.kaggle/kaggle.json
 
 # Download Flickr30K dataset from Kaggle
-!kaggle datasets download -d hsankesara/flickr-image-dataset
-!unzip flickr-image-dataset.zip
-!rm flickr-image-dataset.zip
-!rm -r flickr30k_images/flickr30k_images/flickr30k_images
-!rm flickr30k_images/flickr30k_images/results.csv
+kaggle datasets download -d hsankesara/flickr-image-dataset
+unzip flickr-image-dataset.zip
+rm flickr-image-dataset.zip
+rm -r flickr30k_images/flickr30k_images/flickr30k_images
+rm flickr30k_images/flickr30k_images/results.csv
